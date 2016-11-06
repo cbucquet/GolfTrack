8053968bcf4dcde5e251cb339fa9e9fc7b2c9136
bucquet <bucquet@bucquets-imac.fios-router.home>
2016-11-06 11:41:27 -0500

    Initial Commit

---
+++ GolfTrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GolfTrack
+//
+//  Created by bucquet on 11/6/16.
+//  Copyright © 2016 bucquet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
